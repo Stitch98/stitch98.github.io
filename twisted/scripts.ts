ed4a7f16957291568aa56b2dd01084c4c1d84f45
Stitch98 <stefamito_48@hotmail.it>
2020-04-28 19:58:35 +0200

    formatted both

--- twisted/scripts.ts
+++ twisted/scripts.ts
@@ -1,26 +1,26 @@
 export const BattleScripts: ModdedBattleScriptsData = {
     runSwitch(pokemon: Pokemon) {
-		this.runEvent('Swap', pokemon);
-		this.runEvent('SwitchIn', pokemon);
-		if (this.gen <= 2 && !pokemon.side.faintedThisTurn && pokemon.draggedIn !== this.turn) {
-			this.runEvent('AfterSwitchInSelf', pokemon);
-		}
-		if (!pokemon.hp) return false;
-		pokemon.isStarted = true;
-		if (!pokemon.fainted) {
-            if(pokemon.side.twist && pokemon.isTwist != '0') pokemon.addVolatile('twist'); // here is when twist happens
-			this.singleEvent('Start', pokemon.getAbility(), pokemon.abilityData, pokemon);
-			pokemon.abilityOrder = this.abilityOrder++;
-			this.singleEvent('Start', pokemon.getItem(), pokemon.itemData, pokemon);
-		}
-		if (this.gen === 4) {
-			for (const foeActive of pokemon.side.foe.active) {
-				foeActive.removeVolatile('substitutebroken');
-			}
-		}
-		pokemon.draggedIn = null;
-		return true;
-	},
+        this.runEvent('Swap', pokemon);
+        this.runEvent('SwitchIn', pokemon);
+        if (this.gen <= 2 && !pokemon.side.faintedThisTurn && pokemon.draggedIn !== this.turn) {
+            this.runEvent('AfterSwitchInSelf', pokemon);
+        }
+        if (!pokemon.hp) return false;
+        pokemon.isStarted = true;
+        if (!pokemon.fainted) {
+            if (pokemon.side.twist && pokemon.isTwist != '0') pokemon.addVolatile('twist'); // here is when twist happens
+            this.singleEvent('Start', pokemon.getAbility(), pokemon.abilityData, pokemon);
+            pokemon.abilityOrder = this.abilityOrder++;
+            this.singleEvent('Start', pokemon.getItem(), pokemon.itemData, pokemon);
+        }
+        if (this.gen === 4) {
+            for (const foeActive of pokemon.side.foe.active) {
+                foeActive.removeVolatile('substitutebroken');
+            }
+        }
+        pokemon.draggedIn = null;
+        return true;
+    },
     pokemon: {
         inherit: true,
         isTwist: '0' // This field indicates if the Pokémon has to twist left or right
@@ -34,21 +34,21 @@ export const BattleScripts: ModdedBattleScriptsData = {
             }
             return this.choice.actions.map(action => {
                 switch (action.choice) {
-                case 'move':
-                    let details = ``;
-                    if (action.targetLoc && this.active.length > 1) details += ` ${action.targetLoc > 0 ? '+' : ''}${action.targetLoc}`;
-                    // if (action.mega) details += (action.pokemon!.item === 'ultranecroziumz' ? ` ultra` : ` mega`);
-                    if (action.zmove) details += ` zmove`;
-                    if (action.maxMove) details += ` dynamax`;
-                    return `move ${action.moveid}${details}`;
-                case 'switch':
-                    if (action.mega) details += ` mega`; // As you can see, now MegaEvo happens when you switch, not when you choose a move
-                case 'instaswitch':
-                    return `switch ${action.target!.position + 1}`;
-                case 'team':
-                    return `team ${action.pokemon!.position + 1}`;
-                default:
-                    return action.choice;
+                    case 'move':
+                        let details = ``;
+                        if (action.targetLoc && this.active.length > 1) details += ` ${action.targetLoc > 0 ? '+' : ''}${action.targetLoc}`;
+                        // if (action.mega) details += (action.pokemon!.item === 'ultranecroziumz' ? ` ultra` : ` mega`);
+                        if (action.zmove) details += ` zmove`;
+                        if (action.maxMove) details += ` dynamax`;
+                        return `move ${action.moveid}${details}`;
+                    case 'switch':
+                        if (action.mega) details += ` mega`; // As you can see, now MegaEvo happens when you switch, not when you choose a move
+                    case 'instaswitch':
+                        return `switch ${action.target!.position + 1}`;
+                    case 'team':
+                        return `team ${action.pokemon!.position + 1}`;
+                    default:
+                        return action.choice;
                 }
             }).join(', ');
         },
@@ -56,29 +56,29 @@ export const BattleScripts: ModdedBattleScriptsData = {
             const index = this.getChoiceIndex(true);
             if (index >= this.active.length) return false;
             const pokemon: Pokemon = this.active[index];
-    
+
             switch (this.requestState) {
-            case 'switch':
-                if (pokemon.switchFlag) { // This condition will always happen if called by Battle#choose()
-                    if (!this.choice.forcedPassesLeft) {
-                        return this.emitChoiceError(`Can't pass: You need to switch in a Pokémon to replace ${pokemon.name}`);
+                case 'switch':
+                    if (pokemon.switchFlag) { // This condition will always happen if called by Battle#choose()
+                        if (!this.choice.forcedPassesLeft) {
+                            return this.emitChoiceError(`Can't pass: You need to switch in a Pokémon to replace ${pokemon.name}`);
+                        }
+                        this.choice.forcedPassesLeft--;
                     }
-                    this.choice.forcedPassesLeft--;
-                }
-                break;
-            case 'move':
-                // Checking if the Pokémon has to Twist, no need to specify L or R as they where saved inside the Pokémon
-                if(this.twist){
-                    return this.emitChoiceError(`Can't pass: You chose to Twist, you have to switch.`);
-                }
-                if (!pokemon.fainted) {
-                    return this.emitChoiceError(`Can't pass: Your ${pokemon.name} must make a move (or switch)`);
-                }
-                break;
-            default:
-                return this.emitChoiceError(`Can't pass: Not a move or switch request`);
+                    break;
+                case 'move':
+                    // Checking if the Pokémon has to Twist, no need to specify L or R as they where saved inside the Pokémon
+                    if (this.twist) {
+                        return this.emitChoiceError(`Can't pass: You chose to Twist, you have to switch.`);
+                    }
+                    if (!pokemon.fainted) {
+                        return this.emitChoiceError(`Can't pass: Your ${pokemon.name} must make a move (or switch)`);
+                    }
+                    break;
+                default:
+                    return this.emitChoiceError(`Can't pass: Not a move or switch request`);
             }
-    
+
             // tslint:disable-next-line:no-object-literal-type-assertion
             this.choice.actions.push({
                 choice: 'pass',
@@ -88,22 +88,22 @@ export const BattleScripts: ModdedBattleScriptsData = {
     },
     // The value returned corresponds to wheter or not it is possible to execute Twisting
     // It seemed like it was used only inside the runMegaEvo() function, but I left it like this just in case it is called somewhere else.
-    canMegaEvo(pokemon){
+    canMegaEvo(pokemon) {
         return (pokemon.side.twist) ? false : 'Twist';
     },
     // This function overwrites the normal functioning of the Mega Evolution, so is run when you tick the megaevolution box
     // It activates the side.twist attribute that is checked inside the runSwitch function
     // and it also sets the value for the isTwist attribute inside pokemon, that I'm assuming is preserved after switch in (like canMegaEvo and canDynamax)
     runMegaEvo(pokemon) {
-        if(pokemon.isTwist) return false;
+        if (pokemon.isTwist) return false;
         const side = pokemon.side;
         var i = 0;
         if (side.twist) return false;
         side.twist = true;
         for (const ally of side.pokemon) {
-            if(i % 2 == 0){
+            if (i % 2 == 0) {
                 ally.isTwist = 'L';
-            } else if(i % 2 == 1) {
+            } else if (i % 2 == 1) {
                 ally.isTwist = 'R';
             } i += 1;
         }
--- twisted/statuses.ts
+++ twisted/statuses.ts
@@ -1,14 +1,14 @@
-export const BattleStatuses: {[k: string]: ModdedPureEffectData} = {
+export const BattleStatuses: { [k: string]: ModdedPureEffectData } = {
     inherit: true,
     twist: {
         name: 'Twist',
-		id: 'twist',
-		num: 0,
+        id: 'twist',
+        num: 0,
         duration: 0,
         onStart(pokemon) {
             this.twisted = pokemon.isTwist;
             var twistName;
-            switch(this.twisted){
+            switch (this.twisted) {
                 case 'L':
                     twistName = 'Left Twist';
                     break;
@@ -16,15 +16,15 @@ export const BattleStatuses: {[k: string]: ModdedPureEffectData} = {
                     twistName = 'Right Twist';
                     break;
                 case '0':
-                default: 
+                default:
                     pokemon.removeVolatile('twist'); return;
-                    
+
             }
-            this.add('-start', pokemon,  twistName);
-			if(pokemon.isTwist != '0') this.add('-formechange', pokemon, true);
+            this.add('-start', pokemon, twistName);
+            if (pokemon.isTwist != '0') this.add('-formechange', pokemon, true);
             const side = pokemon.side;
             for (const ally of side.pokemon) {
-                if(ally.isTwist != '0') ally.isTwist = '0';
+                if (ally.isTwist != '0') ally.isTwist = '0';
             }
         },
         onTypePriority: 1,
@@ -33,53 +33,53 @@ export const BattleStatuses: {[k: string]: ModdedPureEffectData} = {
             let twistedTypes: string[] | undefined = [this.getTwistedType(types[0], pokemon.isTwist), this.getTwistedType(types[1], pokemon.isTwist)];
             return twistedTypes;
         },
-        onBeforeMove(move, pokemon){
-            if(pokemon.volatiles['twist'] && pokemon.type === move.type){
+        onBeforeMove(move, pokemon) {
+            if (pokemon.volatiles['twist'] && pokemon.type === move.type) {
                 move.name = this.TwistedTypes[move.type].prefix + ' ' + move.name;
             }
-            
+
         },
         onModifyMove(move, pokemon) {
-            if(pokemon.volatiles['twist'] && pokemon.type === move.type){
+            if (pokemon.volatiles['twist'] && pokemon.type === move.type) {
                 move.type = this.getTwistedType(this.twisted, move.type);
             }
 
         },
         onBeforeSwitchOutPriority: -1,
-		onBeforeSwitchOut(pokemon) {
+        onBeforeSwitchOut(pokemon) {
             pokemon.removeVolatile('twist');
             pokemon.isTwist = '0';
             pokemon.canMegaEvo = null;
         },
         onEnd(pokemon) {
             this.add('-end', pokemon, 'Twist');
-            if(pokemon.isTwist != '0') this.add('-formechange', pokemon, pokemon.species.name);
+            if (pokemon.isTwist != '0') this.add('-formechange', pokemon, pokemon.species.name);
             pokemon.types = pokemon.baseSpecies.types;
             pokemon.side.twist = false;
-		},
+        },
         twisted: '0',
-        getTwistedType(type, lr){
+        getTwistedType(type, lr) {
             return this.TwistedTypes[type][lr];
-        }, 
+        },
         TwistedTypes: {
-            Grass:      {L: 'Rock',     R: 'Electric',  prefix: 'Sprouting'},
-            Fire:       {L: 'Grass',    R: 'Fighting',  prefix: 'Blazing'},
-            Water:      {L: 'Fire',     R: 'Poison',    prefix: 'Soaking'},
-            Electric:   {L: 'Flying',   R: 'Ice',       prefix: 'Sparkling'},
-            Psychic:    {L: 'Dark',     R: 'Bug',       prefix: 'Mesmerizing'},
-            Ice:        {L: 'Psychic',  R: 'Electric',  prefix: 'Freezing'},
-            Dragon:     {L: 'Fairy',    R: 'Fire',      prefix: 'Roaring'},
-            Dark:       {L: 'Ghost',    R: 'Fairy',     prefix: 'Obscuring'},
-            Fairy:      {L: 'Water',    R: 'Ground',    prefix: 'Twinkling'},
-            Normal:     {L: 'Poison',   R: 'Ghost',     prefix: 'Stomping'},
-            Fighting:   {L: 'Steel',    R: 'Dark',      prefix: 'Blasting'},
-            Flying:     {L: 'Rock',     R: 'Ground',    prefix: 'Swirling'},
-            Poison:     {L: 'Bug',      R: 'Grass',     prefix: 'Polluting'},
-            Ground:     {L: 'Fighting', R: 'Ice',       prefix: 'Desolating'},
-            Rock:       {L: 'Dragon',   R: 'Steel',     prefix: 'Crumbling'},
-            Bug:        {L: 'Water',    R: 'Psychic',   prefix: 'Infesting'},
-            Ghost:      {L: 'Normal',   R: 'Flying',    prefix: 'Terrifying'},
-            Steel:      {L: 'Dragon',   R: 'Normal',    prefix: 'Piercing'}
+            Grass: { L: 'Rock', R: 'Electric', prefix: 'Sprouting' },
+            Fire: { L: 'Grass', R: 'Fighting', prefix: 'Blazing' },
+            Water: { L: 'Fire', R: 'Poison', prefix: 'Soaking' },
+            Electric: { L: 'Flying', R: 'Ice', prefix: 'Sparkling' },
+            Psychic: { L: 'Dark', R: 'Bug', prefix: 'Mesmerizing' },
+            Ice: { L: 'Psychic', R: 'Electric', prefix: 'Freezing' },
+            Dragon: { L: 'Fairy', R: 'Fire', prefix: 'Roaring' },
+            Dark: { L: 'Ghost', R: 'Fairy', prefix: 'Obscuring' },
+            Fairy: { L: 'Water', R: 'Ground', prefix: 'Twinkling' },
+            Normal: { L: 'Poison', R: 'Ghost', prefix: 'Stomping' },
+            Fighting: { L: 'Steel', R: 'Dark', prefix: 'Blasting' },
+            Flying: { L: 'Rock', R: 'Ground', prefix: 'Swirling' },
+            Poison: { L: 'Bug', R: 'Grass', prefix: 'Polluting' },
+            Ground: { L: 'Fighting', R: 'Ice', prefix: 'Desolating' },
+            Rock: { L: 'Dragon', R: 'Steel', prefix: 'Crumbling' },
+            Bug: { L: 'Water', R: 'Psychic', prefix: 'Infesting' },
+            Ghost: { L: 'Normal', R: 'Flying', prefix: 'Terrifying' },
+            Steel: { L: 'Dragon', R: 'Normal', prefix: 'Piercing' }
         }
     },
 };
